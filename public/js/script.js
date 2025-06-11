16ca5517ba67b9826ff6e84fce7f60f245201055
neelsshah2006 <neelsshah2006@gmail.com>
2025-06-11 19:52:12 +0530

    Change getCurrentPosition to watchPosition

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,7 @@
 const socket = io();
 
 if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(
+  navigator.geolocation.watchPosition(
     (position) => {
       const { latitude, longitude } = position.coords;
       socket.emit("send-location", { latitude, longitude });
